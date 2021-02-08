27aebce0c342d25c2493974548962dce6376f184
jonsamp <sampjon@gmail.com>
2021-02-07 23:46:24 -0500

    test

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const github = require('@actions/github');
+// const mockGithub = require('./mock-github.json');
 
 function run() {
-  console.log('Running JS...');
-  console.log(JSON.stringify(github));
+  const pullRequest = mockGithub.context.payload.pull_request;
+  const PRBody = pullRequest.body;
+  const PRHref = pullRequest.html_url;
+  const urlRegex = /(https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/g;
+  const urls = PRBody.match(urlRegex) ?? [];
+  const notionUrl = urls.find((url) => url.match('notion.so'));
+
+  console.log({ process, env, secrets });
+
+  if (notionUrl) {
+    const urlParts = notionUrl.split('/');
+    const taskName = urlParts[urlParts.length - 1];
+    const taskParts = taskName.split('-');
+    const pageId = taskParts[taskParts.length - 1];
+    console.log(pageId);
+    console.log(PRHref);
+
+    try {
+      fetch(`https://api.notion.com/v1/pages/${pageId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.NOTION_BOT_SECRET_KEY}`,
+        },
+        body: JSON.stringify({
+          properties: {
+            Status: { name: 'fixready' },
+            'GitHub URL': [{ type: 'text', text: { content: `"${PRHref}"` } }],
+          },
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 run();
